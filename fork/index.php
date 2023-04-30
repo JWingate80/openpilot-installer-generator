27461651101163e676c675ab11f0b89ee3e40715
JWingate80 <john_wingate@hotmail.com>
2023-04-30 12:06:09 -0400

    Adding jwingate alias

--- fork/index.php
+++ fork/index.php
@@ -55,7 +55,8 @@ class Alias {
 $aliases = [new Alias("dragonpilot-community", "release3", ["dragonpilot", "dp"], "", "dragonpilot"),
             new Alias("commaai", DEFAULT_STOCK_BRANCH, ["stock", "commaai"], "", "openpilot"),
             new Alias("sshane", "SA-master", ["shane", "smiskol", "sa", "sshane"], "", "Stock Additions"),
-	    new Alias("sunnyhaibin", "prod-c3", ["sunnypilot", "sp", "sunnyhaibin"], "", "sunnypilot")];
+	    new Alias("sunnyhaibin", "prod-c3", ["sunnypilot", "sp", "sunnyhaibin"], "", "sunnypilot"),
+        new Alias("jwingate80", "test-c3", ["sunnypilot", "sp", "jwingate80"], "", "sunnypilot")];
 foreach ($aliases as $al) {
     if (in_array($username, $al->aliases)) {
         $username = $al->name;
