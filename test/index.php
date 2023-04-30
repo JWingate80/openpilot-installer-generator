559a1dadb2ea2a342587f5c341b409fc0655dd8c
JWingate80 <john_wingate@hotmail.com>
2023-04-30 14:23:11 -0400

    added test page

---
+++ test/index.php
@@ -0,0 +1,12 @@
+<?php
+# Draws visual elements for website
+echo '<head>
+
+</head>';
+
+echo '<html>
+    <body>
+    <h5>Or enter this URL on the setup screen on your device.</h5>
+    </body>
+</html>';
+?>
